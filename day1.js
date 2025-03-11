926b89fb77dc16db3bbbf129fc33dff320b62d58
Pranjal2059 <Pranjalsharma2059@gmail.com>
2025-03-11 13:14:27 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,105 @@
+
+// // console.log("Bye world") // printf
+// // console.log("Hello world")
+// // console.log("haha")
+// // console.log("hehe")
+// // console.log("huhu")
+// //console.log(1+2)
+
+// // variables vaneko k ho ? --
+
+// let bottle = 'coke' // string
+// let dustbin = "fohorrr"
+
+// const pi = 3.14 // number
+// const boilingPoint = 100
+// const gravity = 9.8
+// // output --> I have coke on my bottle with 100 boiling point
+
+// // boolean 
+// let isNepali = true
+// let isIndian = false
+
+
+// console.log("Mero dustbin ma yo xa k xa ? " + dustbin)
+// console.log(`Mero dustbin ma k xa yo xa ? ${dustbin} `) // string literal
+
+
+// //array 
+
+// let friend1 = "Pranjal"
+// let friend2 = "Aashish"
+// let friend3 = "Aayush"
+
+// let friends = ["Pranjal","Aashish","Aayush"]
+
+// let shoe1 = "Umbro"
+// let shoe2 = "Nike"
+// let shoe3 = "calibar"
+
+// let brands = ["Umbro","Nike","Calibar"]
+// // brands.push("addidas") // last ma halxa
+// // brands.unshift("addidas") // fist ma halxa 
+// // brands.pop() // last ko popout or udaedinxa
+// // brands.shift()
+// brands[1] = "Addidas"
+
+// console.log(brands)
+// // console.log(brands[2])
+
+// let countrys = ["Nepal","India","Pakistan"]
+// console.log(countrys.length)
+// // output --> ["America","Ukraine","Russia"]
+
+// // let nums = [1,2,3,4,5,6,7,8]
+
+// // // slice  --> just cut only / org array modify gardainw
+// // let newNums = nums.slice(1,4)
+// // let evenNums = [2,4,6,8,10,12]
+// // let newEvenNums = evenNums.slice(3,6)
+// // console.log(newEvenNums)
+// // console.log(evenNums)
+// // output --> [8,10,12]
+
+
+// console.log(newNums)
+// // splice  -- cut garxa + content halna pani milxa , org array lai modify garxa
+
+let nums = [1,2,3,4,5,6]
+nums.splice(1,2,"Manish")
+console.log(nums)
+
+let numss = [1,2,3,4,5]
+numss.splice(0,5,"a","e","i","o","u")
+console.log(numss)
+// ouput - ["a","e","i","o","u"]
+
+// // split
+let message = "I am from nepal"
+let newMessage = message.split(" ")
+console.log(newMessage)
+let haha = "I,am,nepali"
+let newHaha = haha.split("")
+
+// //object
+// let name = "Manish BaSnet"
+// let age = 22 
+// let address = "Dharan"
+
+// let me = {
+//     name : "Manish BaSnet", 
+//     age : 22, 
+//     address : "Dharan"
+// }
+
+// let country = "Nepal"
+// let language = "Nepali"
+// let region = "Asia"
+// let isLandLocked = true
+
+// let about = {
+//     country : "Nepal", 
+//     language : "Nepali", 
+//     region : "Asia", 
+//     isLandLocked : true
+// }
